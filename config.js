0a37208487b007415b999a81cee38b7d36e7e6e4
btcgamer <datogringo@gmail.com>
2026-08-26 16:17:47 +0400

    Fix Supabase project URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://vtdhvsfqhwesxtwmduew.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://vtdhvsfqhwesxtwmduew.supabase.co";
 const SUPABASE_KEY = "sb_publishable_MnrM2ulyJY_ugwfFVfpQYA_iV5wjCmt";
