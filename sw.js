4ed520c9e1029d80d4a3b32c75d202273c7196f7
btcgamer <datogringo@gmail.com>
2026-08-29 22:33:05 +0400

    Refresh service worker cache after teacher dashboard fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='english-with-mariami-v1';
+const CACHE_NAME='english-with-mariami-v2';
 const APP_SHELL=['./','./index.html','./academy.html','./grade2.html','./grade3.html','./grade4.html','./login.html','./parent-space.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
