ff278227c7e401913562708134e6b8513b5daaf7
btcgamer <datogringo@gmail.com>
2026-08-29 17:52:37 +0400

    Load teacher student management controls on teacher dashboard

--- config.js
+++ config.js
@@ -120,6 +120,7 @@ const SUPABASE_ANON_KEY = 'sb_publishable_MnrM2ulyJY_ugwfFVfpQYA_iV5wjCmt';
       add('logout.js?v=20260829-5');
       add('teacher-navigation.js?v=20260829-6');
       if(path.endsWith('/grade3.html')) add('grade3-progress.js?v=20260829-2');
+      if(path.endsWith('/teacher-dashboard.html')) add('teacher-student-management.js?v=20260829-1');
     }
     if(path.endsWith('/academy.html')){
       const style=document.createElement('style');
